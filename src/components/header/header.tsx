c9e3c5e8daf1bf021fa697429a4c39fe5bdae2c4
Jose <utti222001@utvco.edu.mx>
2026-01-31 16:06:58 -0600

    Nav mejorado

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,35 @@
-import './header.css'
+import "./header.css";
+import { motion, useScroll } from "motion/react";
 
 export default function Header() {
+  const { scrollYProgress } = useScroll();
   return (
     <nav>
+      <motion.div
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 10,
+          originX: 0,
+          backgroundColor: "#187537",
+          zoom: "0.6",
+        }}
+      />
+      <div className="container">
         <h1>José Domínguez</h1>
         <div className="sections">
-            <a>INICIO</a>
-            <a>SOBRE MI</a>
-            <a>PROYECTOS</a>
-            <a>DISEÑOS</a>
+          <a>INICIO</a>
+          <a>SOBRE MI</a>
+          <a>PROYECTOS</a>
+          <a>DISEÑOS</a>
         </div>
         <div className="contacto">
-            <button>Contacto</button>
+          <button>Contacto</button>
         </div>
+      </div>
     </nav>
   );
 }
